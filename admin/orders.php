83ad61bd580d819d83f0da1b6ac9154bc0773305
muhammad-taha-786 <tsyed6804@gmail.com>
2026-02-08 20:30:39 +0500

    orders

--- admin/orders.php
+++ admin/orders.php
@@ -3,12 +3,81 @@
 <div class="container-fluid px-4">
         <div class="card mt-4 shadow-sm">
             <div class="card-header">
-                <h4 class="mb-0">Orders</h4>
+                <div class="row">
+                    <div class="col-md-4">
+                        <h4 class="mb-0">Orders</h4>
+                    </div>
+                    <div class="col-md-8">
+                        <form action="" method="GET">
+                            <div class="row g-1">
+                                <div class="col-md-4">
+                                    <input type="date"
+                                     name="date" 
+                                     class="form-control"
+                                     value="<?= isset($_GET['date']) == true ? $_GET['date']: ''; ?>">
+                                </div>
+                                <div class="col-md-4">
+                                    <select name="payment_status" class="form-select">
+                                        <option value="">Select Payment Status</option>
+
+                                        <option 
+                                        value="Cash Payment"
+                                        <?= isset($_GET['payment_status']) == true ?
+                                         ($_GET['payment_status'] == 'Cash Payment' ? 'selected':'')
+                                         :
+                                         '';
+                                        ?>
+
+                                        >
+                                        Cash Payment
+                                    </option>
+                                        <option value="Online Payment"
+                                        <?= isset($_GET['payment_status']) == true ?
+                                         ($_GET['payment_status'] == 'Online Payment' ? 'selected':'')
+                                         :
+                                         '';
+                                        ?>
+                                        >Online Payment</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <button type="submit" class="btn btn-primary">Filter</button>
+                                    <a href="orders.php" class="btn btn-danger">Reset</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
             <div class="card-body">
 
             <?php 
-                $query = "SELECT o.*, c.* FROM orders o, customers c WHERE c.id = o.customer_id ORDER BY o.id DESC";  ;
+
+                if(isset($_GET['date']) || isset($_GET['payment_status'])){
+                $orderDate = validate($_GET['date']);
+                $paymentStatus = validate($_GET['payment_status']);
+
+                if($orderDate != '' && $paymentStatus != ''){
+                    $query = "SELECT o.*, c.* FROM orders o, customers c 
+                    WHERE c.id = o.customer_id AND o.order_date='$orderDate' ORDER BY o.id DESC";  
+                }elseif($orderDate == '' && $paymentStatus != ''){
+                    $query = "SELECT o.*, c.* FROM orders o, customers c 
+                    WHERE c.id = o.customer_id AND o.payment_mode='$paymentStatus' ORDER BY o.id DESC";  
+                }elseif($orderDate != '' && $paymentStatus != ''){
+                    $query = "SELECT o.*, c.* FROM orders o, customers c 
+                    WHERE c.id = o.customer_id 
+                    AND o.order_date='$orderDate' 
+                    AND o.payment_mode='$paymentStatus' 
+                    ORDER BY o.id DESC";  
+                }else{
+                    $query = "SELECT o.*, c.* FROM orders o, customers c WHERE c.id = o.customer_id ORDER BY o.id DESC";  
+                }
+
+                }else{
+
+                    $query = "SELECT o.*, c.* FROM orders o, customers c WHERE c.id = o.customer_id ORDER BY o.id DESC";  
+                }
+
                 $orders = mysqli_query($conn, $query);
                 if($orders){
                     if(mysqli_num_rows($orders) > 0)
